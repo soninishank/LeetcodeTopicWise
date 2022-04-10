418401516591556d1ee50a558e61f541e96ada56
nishank <akhilkalari@Akhils-MacBook-Pro.local>
2022-04-10 09:57:59 -0700

    Monotonic stack

---
+++ src/Stack/MonotonicStack/Nearest/NearestGreaterToRight.java
@@ -0,0 +1,25 @@
+package Stack.MonotonicStack.Nearest;
+
+import java.util.Stack;
+
+// next larger element
+// https://youtu.be/NXOOYYwpbg4
+// https://leetcode.com/problems/next-greater-element-i/
+public class NearestGreaterToRight {
+    private int[] nearestGreaterToRight(int[] arr) {
+        int[] result = new int[arr.length];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = arr.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && arr[stack.peek()] <= arr[i]) {
+                stack.pop();
+            }
+            if (stack.isEmpty()) {
+                result[i] = 0;
+            } else {
+                result[i] = stack.peek() - i;
+            }
+            stack.push(i);
+        }
+        return result;
+    }
+}
