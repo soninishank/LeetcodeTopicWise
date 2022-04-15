a4099f121c850b47c53d57fedba8ed9ec8dd4e3f
nishank <akhilkalari@Akhils-MacBook-Pro.local>
2022-04-15 04:58:00 -0700

    LCA

--- src/Tree/LCA/LCABinarySearchTree.java
+++ src/Tree/LCA/LCABinarySearchTree.java
@@ -1,2 +1,17 @@
-package Tree.LCA;public class LCABinarySearchTree {
+package Tree.LCA;
+
+import Tree.TreeNode;
+
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+public class LCABinarySearchTree {
+    public TreeNode lowestCommonAncestorBST(TreeNode root, TreeNode p, TreeNode q) {
+        // remember to firstly write this conditions
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestorBST(root.left, p, q);
+        } else if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestorBST(root.right, p, q);
+        } else {
+            return root;
+        }
+    }
 }
