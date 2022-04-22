10265bd25435f21051e7c746087e4a6e0474d556
nishank <akhilkalari@Akhils-MacBook-Pro.local>
2022-04-22 08:02:31 -0700

    Array Imp Questions

---
+++ src/Array/ProductOfArrayExceptSelf.java
@@ -0,0 +1,21 @@
+package Array;
+
+// https://leetcode.com/problems/product-of-array-except-self/
+public class ProductOfArrayExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+        int[] leftSide = new int[nums.length];
+        int[] rightSide = new int[nums.length];
+
+        leftSide[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            leftSide[i] = leftSide[i - 1] * nums[i - 1];
+        }
+
+        rightSide[nums.length - 1] = 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            rightSide[i] = nums[i + 1] * rightSide[i + 1];
+            rightSide[i + 1] = rightSide[i + 1] * leftSide[i + 1];
+        }
+        return rightSide;
+    }
+}
